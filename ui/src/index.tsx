a7944dcbd1a365a548667e50204c4e7c181c5056
Daniel Moloney <daniel.moloney@sysdig.com>
2023-03-08 20:18:59 +1100

    updates

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -1,10 +1,32 @@
 import * as React from 'react';
+import { useState, useEffect } from "react";
+
+type resultProps = {
+  email: string;
+  gender: string;
+};
 
 export const Extension = (props: {
   tree: any;
   resource: any }) => {
 
   console.log(props);
+
+  const [result, setResult] = useState<resultProps[]>([]);
+
+  useEffect(() => {
+    const api = async () => {
+      const data = await fetch("https://randomuser.me/api", {
+        method: "GET"
+      });
+      const jsonData = await data.json();
+      setResult(jsonData.results);
+    };
+
+    api();
+  }, []);
+
+  console.log(result);
 
   return (
     <div>Hello {props.resource.metadata.name}!</div>
