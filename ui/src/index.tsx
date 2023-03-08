76cd0de24df0a2bd65bc140b23f74ba4731f8ca6
Daniel Moloney <daniel.moloney@sysdig.com>
2023-03-08 21:08:58 +1100

    updates

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -1,17 +1,17 @@
 import * as React from 'react';
-import axios from "axios";
+// import axios from "axios";
 
 // const baseURL = `https://randomuser.me/api`;
 const baseURL = 'https://app.au1.sysdig.com/api/scanning/runtime/v2/workflows/results?cursor&filter=kubernetes.namespace.name="sock-shop"&limit=100&order=desc&sort=runningVulnsBySev';
 
-let reqInstance = axios.create({
-  method: 'get',
-  withCredentials: false,
-  headers: {
-    // Authorization: `Bearer ${localStorage.getItem("access_token")}`
-    Authorization: 'Bearer f9bc946b-a894-406f-a563-98474047b5c7'
-  }
-})
+// let reqInstance = axios.create({
+//   method: 'get',
+//   withCredentials: false,
+//   headers: {
+//     // Authorization: `Bearer ${localStorage.getItem("access_token")}`
+//     Authorization: 'Bearer f9bc946b-a894-406f-a563-98474047b5c7'
+//   }
+// })
 
 export const Extension = (props: {
   tree: any;
@@ -20,10 +20,21 @@ export const Extension = (props: {
   const [post, setPost] = React.useState(null);
 
   React.useEffect(() => {
-    reqInstance(baseURL).then((response: { data: any; }) => {
-      setPost(response.data);
+    const data = fetch(baseURL, {
+      method: 'GET',
+      mode: 'no-cors',
+      headers: {
+        Authorization: 'Bearer f9bc946b-a894-406f-a563-98474047b5c7',
+      }
     });
+    setPost(data);
   }, []);
+
+  // React.useEffect(() => {
+  //   reqInstance(baseURL).then((response: { data: any; }) => {
+  //     setPost(response.data);
+  //   });
+  // }, []);
 
   if (!post) return null;
 
