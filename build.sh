fd38b51767673a31d62c31677552482c055531e7
Daniel Moloney <daniel.moloney@sysdig.com>
2023-03-09 10:56:56 +1100

    updates

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # rm old builds
-rm -rf ui/dist/
-rm -rf resources/
+rm -rf ./ui/dist/
+rm -rf ./resources/
 rm -f ./extension.tar
 
 # lets build
@@ -11,7 +11,7 @@ yarn build
 
 # setup for psuh
 sleep 5
-cp -rf ui/dist/* ./
+cp -rf ./ui/dist/* ./
 
 # git commit
 cd ..
