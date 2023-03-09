4ea9e7f1eab4cb1a1c2917ac50e8afca5bf3bdba
Daniel Moloney <daniel.moloney@sysdig.com>
2023-03-09 11:56:38 +1100

    updates

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -72,6 +72,8 @@ export const Extension = (props: {
           fontWeight: "bold",
           textTransform: "uppercase",
           textAlign: "center",
+          marginTop: "15px",
+          marginBottom: "15px",
         }}
       >
         Sysdig Vulnerability Report - Application {props.resource.metadata.name}</div>
@@ -82,7 +84,8 @@ export const Extension = (props: {
           boxShadow: "1px 1px 1px #ccd6dd",
           borderRadius: "4px",
           border: "1px solid transparent",
-          margin: "15px",
+          marginTop: "15px",
+          marginBottom: "15px",
         }}
       >
         <div
