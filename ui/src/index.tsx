c6c7a072478f078eccb3c195c44ee7bcc8a2e554
Daniel Moloney <daniel.moloney@sysdig.com>
2023-03-09 12:51:40 +1100

    updates

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import Moment from "react-moment";
 
 const MAP_VULN = {
   Critical: {
@@ -121,6 +122,92 @@ export const Extension = (props: {
             </div>
           ))}
         </div>
+      </div>
+
+      <div style={{ display: "flex", flexDirection: "column", width: "40%" }}>
+        {data.map((key) => (
+          <div
+            title={`Kind: Application Namespace: ${key.namespace} Name: ${key.name}`}
+            style={{
+              marginTop: "2rem",
+              padding: "0.2rem",
+              boxShadow: "1px 1px 1px #ccd6dd",
+              borderRadius: "4px",
+              border: "1px solid transparent",
+              backgroundColor: "#fff",
+              color: "#363c4a",
+              display: "flex",
+            }}
+          >
+            <div
+              style={{
+                width: "60px",
+                flexGrow: "1",
+                color: "#495763",
+                textAlign: "center",
+              }}
+            >
+              <i title="Application" className="icon argo-icon-application"></i>
+              <br />
+              <div style={{ fontSize: ".7em", color: "#6d7f8b" }}>
+                application
+              </div>
+            </div>
+            <div
+              style={{
+                flexGrow: "100",
+                padding: "10px 20px 10px 10px",
+                lineHeight: ".95",
+                display: "flex",
+                flexDirection: "column",
+              }}
+            >
+              <div
+                style={{
+                  fontSize: ".8em",
+                  paddingBottom: "5px",
+                  overflow: "hidden",
+                  textOverflow: "ellipsis",
+                  textAlign: "left",
+                }}
+              >
+                {key.name}
+              </div>
+              <div style={{ fontSize: ".8em" }}>
+                <i
+                  qe-id="utils-health-status-title"
+                  title={key.health.status}
+                  className={`fa ${MAP_VULN[key.health.status].name}${MAP_VULN[key.health.status].spin ? " fa-spin" : ""
+                    }`}
+                  style={{ color: MAP_VULN[key.health.status].color, marginRight: "0.3rem" }}
+                ></i>
+                <a href={`/applications/${key.name}`} title="Open application">
+                  <i className="fa fa-external-link-alt"></i>
+                </a>
+              </div>
+            </div>
+            <div style={{ flexGrow: "1", alignSelf: "flex-end" }}>
+              {key.createdAt ? (
+                <Moment
+                  style={{
+                    backgroundColor: "#ccd6dd",
+                    color: "#495763",
+                    border: "1px solid #8fa4b1",
+                    borderRadius: "5px",
+                    padding: "0 5px",
+                    fontSize: ".6em",
+                    textTransform: "lowercase",
+                    marginRight: "1px",
+                  }}
+                  fromNow={true}
+                  ago={true}
+                >
+                  {item.createdAt}
+                </Moment>
+              ) : null}
+            </div>
+          </div>
+        ))}
       </div>
 
       <div
